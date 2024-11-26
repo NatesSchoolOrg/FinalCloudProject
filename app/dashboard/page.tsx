bf48d326fcc43182b559828737540c65420a5b8e
Jimmy German <jgjimmy1@gmail.com>
2024-11-25 19:50:20 -0500

    Worked on Home Page Co-authored-by: Nate-Louder <Nate-Louder@users.noreply.github.com>

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,22 +6,18 @@ import Dashboard from "./Dashboard";
 import HolidaySelector from "./HolidaySelector";
 const DashboardPage = () => {
     const [selectedHoliday, setSelectedHoliday] = useState<Holiday | undefined>(undefined);
-    // useProtectedRoute();
-    
+    useProtectedRoute();
+
     const onHolidaySelect = (holiday: Holiday) => {
         setSelectedHoliday(holiday);
-    }
+    };
     return (
         <div>
-            {selectedHoliday ? 
-                <Dashboard 
-                    selectedHoliday={selectedHoliday} 
-                    onClearSelection={() => setSelectedHoliday(undefined)}
-                ></Dashboard> : 
-                <HolidaySelector 
-                    holidays={allHolidayData} 
-                    onHolidaySelect={onHolidaySelect} 
-                />}
+            {selectedHoliday ? (
+                <Dashboard selectedHoliday={selectedHoliday} onClearSelection={() => setSelectedHoliday(undefined)}></Dashboard>
+            ) : (
+                <HolidaySelector holidays={allHolidayData} onHolidaySelect={onHolidaySelect} />
+            )}
         </div>
     );
 };
