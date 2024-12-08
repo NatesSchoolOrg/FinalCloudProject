386253908ee5fa99d9b4ef6ce075f66d8fa1c190
Nate-Louder <nmlouder@gmail.com>
2024-12-07 19:29:34 -0500

    deploy

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -14,7 +14,6 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(Cookies.get("loggedIn") === "true");
-
   const isSecure = process.env.NODE_ENV === "production";
 
   useEffect(() => {
